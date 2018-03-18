3fbd93c769105e44d153ec512d5880ca2c4233fc
linuxiseasy <linuxsh8@gmail.com>
2018-03-18 08:03:21 -0400

    Initial Commit

---
+++ FAucG.js
@@ -0,0 +1,34 @@
+{
+    "items": [
+        {
+            "name": "Crawdad",
+            "description": "It's actually a \"crayfish\"",
+            "photo": "https://dummyimage.com/150x150/000/fff",
+            "price": 4.50
+        },
+        {
+            "name": "Gorilla",
+            "description": "Gives a friendly wave.",
+            "photo": "https://dummyimage.com/150x150/000/fff",
+            "price": 8.50
+        },
+        {
+            "name": "Ninja",
+            "description": "Hero in a half shell.",
+            "photo": "https://dummyimage.com/150x150/000/fff",
+            "price": 12.50
+        },
+        {
+            "name": "Psion",
+            "description": "Computing Classic.",
+            "photo": "https://dummyimage.com/150x150/000/fff",
+            "price": 110.00
+        },
+        {
+            "name": "Totem",
+            "description": "Mysterious and wooden.",
+            "photo": "https://dummyimage.com/150x150/000/fff",
+            "price": 150.00
+        }
+    ]
+}
